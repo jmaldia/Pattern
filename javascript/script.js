e45fa3b40e49732094debe73144e2b3e16e9af0a
Jon Maldia <jon@maldia.net>
2019-10-25 10:05:53 -0400

    Add text for exercise

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+let firstName = ''; 
+let lastName = ''; 
+let age = 0;
+
+firstName = prompt("What's your first name?");
+lastName = prompt("What's your last name?");
+age = prompt("What's your age?");
+
+console.log(`Your name is ${firstName} ${lastName}.`);
+console.log(`You are ${age} years old.`);
